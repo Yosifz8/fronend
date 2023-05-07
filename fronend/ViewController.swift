65a288c4738ece8940f98c8a4fd311e4786ac834
Yosi Faroh Zada <yosi@motiv8ai.com>
2023-05-07 23:14:29 +0300

    Initial Commit

---
+++ fronend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fronend
+//
+//  Created by Yosi Faroh Zada on 07/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
